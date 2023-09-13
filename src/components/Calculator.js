ec4a75d354bb07f157356f0302d1c3b20444fcd3
Adeshina Adenuga <Nuga0909@gmail.com>
2023-09-13 16:21:18 +0100

    Calculator functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,30 @@
+import React, { useState } from 'react';
 import Buttons from './Buttons';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="container flex">
-    <div className="Output-bar flex button-container"> 0</div>
-    <Buttons />
-  </div>
-);
+const Calculator = () => {
+  const [calculation, setCalculation] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const clickEvent = (e) => {
+    const buttonName = e.currentTarget.textContent;
+    const newState = calculate(calculation, buttonName);
+    setCalculation(newState);
+  };
+
+  // const { next, operation, total } = calculation;
+
+  return (
+    <div className="container flex">
+      <div className="Output-bar flex button-container">
+        {calculation.next || calculation.operation || calculation.total || '0'}
+      </div>
+      <Buttons />
+    </div>
+  );
+};
 
 export default Calculator;

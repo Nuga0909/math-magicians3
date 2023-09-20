9898d992dba677cfdea8fa145702d293609e7bd8
Adeshina Adenuga <Nuga0909@gmail.com>
2023-09-20 11:19:17 +0100

    fix linter errors

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,36 +2,36 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    const navLinks = [
-        {
-            id: 1,
-            name: 'Home',
-            path: '/',
-        },
-        {
-            id: 2,
-            name: 'Calculator',
-            path: '/calculator',
-        },
-        {
-            id: 3,
-            name: 'Quote',
-            path: '/quote',
-        },
-    ];
+  const navLinks = [
+    {
+      id: 1,
+      name: 'Home',
+      path: '/',
+    },
+    {
+      id: 2,
+      name: 'Calculator',
+      path: '/calculator',
+    },
+    {
+      id: 3,
+      name: 'Quote',
+      path: '/quote',
+    },
+  ];
 
-    return (
-        <nav className="NavItems flex">
-            <h3>Math Magicians</h3>
-            <ul>
-                {navLinks.map(({ id, name, path }) => (
-                    <li key={id}>
-                        <NavLink to={path}>{name}</NavLink>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="NavItems flex">
+      <h3>Math Magicians</h3>
+      <ul>
+        {navLinks.map(({ id, name, path }) => (
+          <li key={id}>
+            <NavLink to={path}>{name}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
